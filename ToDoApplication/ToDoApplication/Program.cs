982c1f53851b27a78fb00163c4ce1ff17f384a72
Albert Bach <bach_albert@hotmail.hu>
2017-09-28 11:42:01 +0200

    First usage finished and working

---
+++ ToDoApplication/ToDoApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApplication
+{
+    class ToDoApplication
+    {
+        static void Main(string[] args)
+        {
+            Opening();
+            Console.ReadLine();
+        }
+
+        public static void Opening()
+            {
+            Console.WriteLine("Command Line Todo application\n" +
+                "=============================\n" +
+                "Command line arguments:\n" +
+                " -l Lists all the tasks\n" +
+                " -a Adds a new task\n" +
+                " -r Removes an task\n" +
+                " -c Completes an task");
+        }
+    }
+}
+             
